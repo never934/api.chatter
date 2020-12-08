d7e8a9a9cde685845a746fefce35d8afa5e44adb
never934 <48915853+never934@users.noreply.github.com>
2020-12-08 22:42:34 +0300

    views+

---
+++ src/main/kotlin/com/tastyeem/apichatter/models/view/ErrorView.kt
@@ -0,0 +1,1 @@
+package com.tastyeem.apichatter.models.viewdata class ErrorView(    val message: String)
---
+++ src/main/kotlin/com/tastyeem/apichatter/utils/TokenUtils.kt
@@ -0,0 +1,1 @@
+package com.tastyeem.apichatter.utilsimport com.tastyeem.apichatter.AppConstimport java.util.*class TokenUtils {}
